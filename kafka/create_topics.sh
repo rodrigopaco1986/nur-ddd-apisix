63bab8b37482f3f99068e6c15ea38f842a396004
Rodrigo Paco <rodrigopaco.1986@gmail.com>
2025-06-14 01:40:01 -0400

    added services topic for kafka broker

--- kafka/create_topics.sh
+++ kafka/create_topics.sh
@@ -5,7 +5,7 @@ echo "Creating topics…"
 
 
 # Create topics (ignore if they already exist)
-#-----------CLIENTS---------
+#-----------PATIENTS---------
 kafka-topics.sh \
   --bootstrap-server kafka:9092 \
   --create \
@@ -26,6 +26,31 @@ kafka-topics.sh \
   --bootstrap-server kafka:9092 \
   --create \
   --topic patient.deleted \
+  --partitions 1 \
+  --replication-factor 1 \
+  || true
+
+#-----------SERVICES---------
+kafka-topics.sh \
+  --bootstrap-server kafka:9092 \
+  --create \
+  --topic service.created \
+  --partitions 1 \
+  --replication-factor 1 \
+  || true
+
+kafka-topics.sh \
+  --bootstrap-server kafka:9092 \
+  --create \
+  --topic service.updated \
+  --partitions 1 \
+  --replication-factor 1 \
+  || true
+
+kafka-topics.sh \
+  --bootstrap-server kafka:9092 \
+  --create \
+  --topic service.deleted \
   --partitions 1 \
   --replication-factor 1 \
   || true
